012030368f0c7cb9db76d5aab265d15ff11ab297
wataridmm <r.watari.watari@gmail.com>
2021-10-02 14:51:02 +0000

    fix sign_up user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  # belongs_to :books
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
