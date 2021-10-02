430440295c05d9d5b1187f49206417b11275b020
wataridmm <r.watari.watari@gmail.com>
2021-10-02 15:06:35 +0000

    fix appcontroller user_path(resource)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
