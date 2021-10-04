3ef22cfd6a6aacc0091164d0e8482cfbefa47f4d
wataridmm <r.watari.watari@gmail.com>
2021-10-04 05:00:17 +0000

    fix users/index,books/_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.all
+    @books = @user.books
+
     @book = Book.new
   end
 
