296952d261b85bf3fb97637e43e465ee00512294
wataridmm <r.watari.watari@gmail.com>
2021-10-05 14:58:25 +0000

    add follow html routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,17 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   # post 'books/edit' => 'books#edit'
-
+  resources :users do
+    member do
+      get :following, :followers
+           end
+  end
 
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   end
 
 end
