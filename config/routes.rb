bebc88e4757d7a581c8a90ea137104d192c24d70
wataridmm <r.watari.watari@gmail.com>
2021-10-05 13:21:20 +0000

    add relationship model,controller,time stamp.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+  get 'relationships/destroy'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_050927) do
+ActiveRecord::Schema.define(version: 2021_10_05_124145) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_10_05_050927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "book_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
